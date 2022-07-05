61098188a836ad729ecdbbe03ec44be0b11cb9aa
PandaGamerYT <70008216+Zach-Tolentino-161@users.noreply.github.com>
2022-07-05 21:23:26 +0800

    Update to main.py
    This update is to combine read.py and main.py together

--- main.py
+++ main.py
@@ -9,12 +9,25 @@ def sleep(sec):
 def clear():
     subprocess.Popen("cls" if platform.system() == "Windows" else "clear",
                      shell=True)
-hasPicked = input("Do you want to make a note? (n)\n")
-if hasPicked == "n" or "N": 
-        note = input("What is the name of the note?\n")
-        print("If you want to save the note, press s")
-        noteContents = input("Please enter the contents of the note. Please know this doesn't work with multilines. Sorry I am still learning code\n")
-        Data = {"Note name": note, "Note contents": noteContents}
-        db.collection("Data").add(Data)
-        clear()
-        exit()
+hasPicked = input("Do you want to make a note or read a note? (n/r)\n")
+class MakeNote:
+  if hasPicked.lower() == "n" or hasPicked.lower() == "N": 
+          note = input("What is the name of the note? Do not add spaces and symbols. So please instead of symbols/spaces please just combine the letters together.\n")
+          noteContents = input("Please enter the contents of the note. Please note this doesn't work with multilines.\n")
+          Data = {"Note name": note, "Note Contents": noteContents}
+          db.collection("Data").add(Data)
+          print("Saved your note.")
+          exit()
+  elif hasPicked.lower()  == "R" or hasPicked.lower() == "r":
+    docs = db.collection("Data").stream()
+    print("Please enter the name of the note you want to read")
+    note = input()
+    for doc in docs:
+      try:  
+        if doc.to_dict()["Note name"] == note:
+          print(doc.to_dict()["Note contents"])
+    
+      except:
+          print("Could not find the note.")
+          exit(400)
+MakeTheNote = MakeNote()
